87144cd2aeaf7397bf99b2b236adaf55aa8fe5da
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:28 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.lambda.auth/magic.lambda.auth.csproj --configuration Release
+dotnet build magic.lambda.auth/magic.lambda.auth.csproj --configuration Release --source https://api.nuget.org/v3/index.json
